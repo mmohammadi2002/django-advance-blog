b435e30da12eae84e8db8ea94d24eb8aa0d009dc
Mohammad Hosein Mohammadi <Iammohammadmohammadi1381@gmail.com>
2026-09-22 18:43:29 +0330

    Add accounts admin configuration and profile model with signal

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
 # Register your models here.
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'is_superuser', 'is_active')
+    list_filter = ('email', 'is_superuser', 'is_active')
+    search_fields = ('email',)
+    ordering = ('email',)
+
+    fieldsets = (
+        ('Authentication', {"fields": ("email", "password")}),
+        ('Permissions', {"fields": ("is_staff", "is_superuser", "is_active")}),
+        ('Group Permissions', {"fields":("groups", "user_permissions")}),
+        ('Important date', {"fields":("last_login",)})
+    )
+
+    add_fieldsets =(
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_superuser", "is_active"),
+        }),
+    )
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Profile)
+    
